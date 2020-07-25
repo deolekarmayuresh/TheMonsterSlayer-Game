4e435ebc8752c47ac5c642cb6a4fc6189e1b58e3
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-07-25 23:55:48 +0530

    app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ new Vue({
         gameIsRunning: false
     },
     methods: {
-        startGame: () => {
+        startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
