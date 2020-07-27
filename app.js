02263f1cc622192deb973c72df40d3d5d9f6bd8a
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-07-27 12:00:34 +0530

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
@@ -12,7 +13,12 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function() {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            let damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: "Player hits the Monster for" + damage
+            });
             if (this.checkWin()) {
                 return;
             }
@@ -43,7 +49,12 @@ new Vue({
 
         },
         monsterAttack: function() {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            let damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: "Monster hits the player for" + damage
+            });
             this.checkWin();
         },
         calculateDamage: function (min, max) {
